e759068ac58594d79a19934a3b3114655eb0ca67
Viktor <Rickeygame23@gmail.com>
2022-03-17 17:02:01 +0200

    new lesson

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const str = 'Test';
+const arr = [1, 2, 4];
+
+console.log(str.toLowerCase());
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('fruit'));
+
+const logg = 'Hello World!';
+
+console.log(logg.slice(6, 10));
+
+console.log(logg.substr(6, 5));
+
+
+const num = 12.5;
+console.log(Math.round(num));
+
+const test = '12.2px';
+
+console.log(parseFloat(test));
