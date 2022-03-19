6dc417b6899ce2eda1666fef255156f81a527c35
Viktor <Rickeygame23@gmail.com>
2022-03-19 02:52:54 +0200

    Dynamic TypeOf

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 'use strict';
 
-let str = 'some';
-let strObj = new String(str);
+// to String
 
-// console.log(typeof(str));
-// console.log(typeof(strObj));
+// 1)
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
 
-// console.dir([1,2,3]);
+// 2)
+console.log(typeof(5 + ''));
 
-const soldier = {
-    health: 400,
-    armor: 100
-};
+const fontSize = 26 + 'px'
+const num = 5;
+console.log('https://vk.com/catalog/' + num);
 
-const john = Object.create(soldier);
 
-john.health = 100;
+// to Number
+
+// 1)
+console.log(typeof(Number('4')));
+
+// 2)
+console.log(+'5');
+
+// 3)
+console.log(parseInt('15px', 10));
+
+// to Boolean
+
+// 1)
+// 0, '', null, undefined, NaN; - это всё будет false
+
+let switcher = null;
+if(switcher){
+    console.log('working...')
+}
+
+switcher = 1;
+if(switcher){
+    console.log('working...')
+}
+
+// 2)
+console.log(typeof(Boolean('4')));
+
+// 3)
+console.log(typeof(!!'1'));
